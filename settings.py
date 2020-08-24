43e1756cecd0efa4bc932ceaa6b2d5cf1ed8534b
William Funchal <williamfunchal@MacBook-Pro-de-iTech.local>
2020-08-24 12:10:30 -0300

    start order = 300. step orde = 100

--- settings.py
+++ settings.py
@@ -37,8 +37,8 @@ ORDER_PAIRS = 6
 # ORDER_START_SIZE will be the number of contracts submitted on level 1
 # Number of contracts from level 1 to ORDER_PAIRS - 1 will follow the function
 # [ORDER_START_SIZE + ORDER_STEP_SIZE (Level -1)]
-ORDER_START_SIZE = 200
-ORDER_STEP_SIZE = 200
+ORDER_START_SIZE = 300
+ORDER_STEP_SIZE = 100
 
 # Distance between successive orders, as a percentage (example: 0.005 for 0.5%)
 INTERVAL = 0.005
