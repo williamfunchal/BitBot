0d937a7aa2703c58d6e2b3a119ef1fc55fbd292d
William Funchal <williamfunchal@outlook.com>
2020-10-07 19:50:34 -0300

    Interval of Orders: 0.15%. First order value: 100 USD

--- settings.py
+++ settings.py
@@ -37,11 +37,11 @@ ORDER_PAIRS = 6
 # ORDER_START_SIZE will be the number of contracts submitted on level 1
 # Number of contracts from level 1 to ORDER_PAIRS - 1 will follow the function
 # [ORDER_START_SIZE + ORDER_STEP_SIZE (Level -1)]
-ORDER_START_SIZE = 500
+ORDER_START_SIZE = 100
 ORDER_STEP_SIZE = 100
 
 # Distance between successive orders, as a percentage (example: 0.005 for 0.5%)
-INTERVAL = 0.005
+INTERVAL = 0.0015
 
 # Minimum spread to maintain, in percent, between asks & bids
 MIN_SPREAD = 0.01
