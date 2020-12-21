3482849ee5b48e96f83b4d970f29d140b585f12b
William Funchal <williamfunchal@outlook.com>
2020-12-21 11:24:40 -0300

    liquidated. Working with remaining balance

--- settings.py
+++ settings.py
@@ -43,8 +43,8 @@ ORDER_PAIRS = 6
 # ORDER_START_SIZE will be the number of contracts submitted on level 1
 # Number of contracts from level 1 to ORDER_PAIRS - 1 will follow the function
 # [ORDER_START_SIZE + ORDER_STEP_SIZE (Level -1)]
-ORDER_START_SIZE = 5
-ORDER_STEP_SIZE = 10
+ORDER_START_SIZE = 1
+ORDER_STEP_SIZE = 2
 
 # Distance between successive orders, as a percentage (example: 0.005 for 0.5%)
 INTERVAL = 0.0007
