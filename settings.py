04cc61595855721ae590a7e72edd1efd5fbdcfab
William Funchal <williamfunchal@outlook.com>
2021-01-13 18:17:06 -0300

    ORDER_START_SIZE = 10 TARGET_TO_PROFIT = 0.2

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ API_SECRET = "-eQJgYT_D5v264jd0X2QqaLCL1u8eRUkSw4nu0yr2gSPB85u"
 # Instrument to market make on BitMEX.
 SYMBOL = "XBTUSD"
 
-TARGET_TO_PROFIT = 0.5
+TARGET_TO_PROFIT = 0.2
 
 # TAKE_PROFIT_TRIGGER = 3000
 
@@ -43,7 +43,7 @@ ORDER_PAIRS = 8
 # ORDER_START_SIZE will be the number of contracts submitted on level 1
 # Number of contracts from level 1 to ORDER_PAIRS - 1 will follow the function
 # [ORDER_START_SIZE + ORDER_STEP_SIZE (Level -1)]
-ORDER_START_SIZE = 5
+ORDER_START_SIZE = 10
 ORDER_STEP_SIZE = 1
 
 # Distance between successive orders, as a percentage (example: 0.005 for 0.5%)
