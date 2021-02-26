ee2085d906fa586aa73e92134ced952bacb2b0f3
William Funchal <williamfunchal@outlook.com>
2021-02-26 02:07:37 -0300

    reverting

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ TARGET_TO_PROFIT = 0.15
 
 MAINTAIN_ENTRY_PRICE_SPREAD_CENTER = True
 
-POSITION_START_ENTRY_QTY = 3000
+POSITION_START_ENTRY_QTY = 1000
 
 
 ########################################################################################################################
@@ -45,7 +45,7 @@ ORDER_PAIRS = 6
 # ORDER_START_SIZE will be the number of contracts submitted on level 1
 # Number of contracts from level 1 to ORDER_PAIRS - 1 will follow the function
 # [ORDER_START_SIZE + ORDER_STEP_SIZE (Level -1)]
-ORDER_START_SIZE = 600
+ORDER_START_SIZE = 200
 ORDER_STEP_SIZE = 0
 
 # Distance between successive orders, as a percentage (example: 0.005 for 0.5%)
