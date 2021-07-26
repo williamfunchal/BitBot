f28e1271232ac49b4199f3fb5d52c77430cd8ce2
William Funchal <williamfunchal@gmail.com>
2021-07-26 18:43:45 -0300

    leveraging 10 %

--- market_maker/market_maker.py
+++ market_maker/market_maker.py
@@ -604,20 +604,20 @@ class OrderManager:
 
                 if qty > 0: 
                     if buys_matched <= 1:
-                        leverage -= leverage * 0.05
+                        leverage -= leverage * 0.1
                         self.exchange.isolate_margin(self.exchange.symbol, leverage ,True)
 
                     if buys_matched > 2:
-                        leverage += leverage * 0.05
+                        leverage += leverage * 0.1
                         self.exchange.isolate_margin(self.exchange.symbol, leverage ,True)
 
                 if qty < 0: 
                     if sells_matched <= 1:
-                        leverage -= leverage * 0.05
+                        leverage -= leverage * 0.1
                         self.exchange.isolate_margin(self.exchange.symbol,leverage,True)
 
                     if sells_matched > 2:
-                        leverage += leverage * 0.05
+                        leverage += leverage * 0.1
                         self.exchange.isolate_margin(self.exchange.symbol, leverage ,True)
 
 
