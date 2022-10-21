f5dc0e5b83ef7f2808e24b7afc51315d375be213
William Funchal <williamfunchal@gmail.com>
2022-10-21 13:44:08 -0300

    leverage: 85, target: 0,4, order: 200

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ API_SECRET = "-eQJgYT_D5v264jd0X2QqaLCL1u8eRUkSw4nu0yr2gSPB85u"
 # Instrument to market make on BitMEX.
 SYMBOL = "XBTUSD"
 
-LEVERAGE = 100
+LEVERAGE = 85
 
 TARGET_TO_PROFIT = 0.4
 
@@ -48,8 +48,8 @@ ORDER_PAIRS = 4
 # [ORDER_START_SIZE + ORDER_STEP_SIZE (Level -1)]
 # ORDER_START_SIZE = 200
 # ORDER_STEP_SIZE = 200
-ORDER_START_SIZE = 100
-ORDER_STEP_SIZE = 100
+ORDER_START_SIZE = 200
+ORDER_STEP_SIZE = 200
 
 # Distance between successive orders, as a percentage (example: 0.005 for 0.5%)
 INTERVAL = 0.0050
