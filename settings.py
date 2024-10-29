9b4978427d4abaef3f49045fd0c1bca64fbc8917
William Funchal <williamfunchal@gmail.com>
2024-10-29 18:51:58 -0300

    LAST CHANGES

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from os.path import join
 import logging
+import os
 
 ########################################################################################################################
 # Connection/Auth
@@ -11,11 +12,8 @@ BASE_URL = "https://www.bitmex.com/api/v1/" # Once you're ready, uncomment this.
 BASE_WS_URL = "wss://www.bitmex.com/realtime" # Once you're ready, uncomment this.
 
 # The BitMEX API requires permanent API keys. Go to https://testnet.bitmex.com/app/apiKeys to fill these out.
-API_KEY = "k1FTMOrfBR_4H0z4DKSxJLQl"
-API_SECRET = "-eQJgYT_D5v264jd0X2QqaLCL1u8eRUkSw4nu0yr2gSPB85u"
-
-# API_KEY = "m42_T2utX8zm_v7v-9_7F2Lt"
-# API_SECRET = "vOIYOG745fuPy4WxmUAS-PLYYZYDcnIHvnpPkWt_F9bqND_r"
+API_KEY = os.environ.get('BITMEX_API_KEY')
+API_SECRET = os.environ.get('BITMEX_API_SECRET')
 
 
 ########################################################################################################################
